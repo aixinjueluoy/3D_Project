798571976ef04ff020c5bb056388ffa706a5d4ed
aixinjueluoy <ycx2931380491@163.com>
2025-08-25 22:24:45 +0800

    hitReaction

--- Assets/Scripts/Combat System/MeeleFighter.cs
+++ Assets/Scripts/Combat System/MeeleFighter.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MeeleFighter : MonoBehaviour
 {
+    [SerializeField] private GameObject sword;
     public bool InAction { get; private set; } = false;
 
     private Animator anim;
 
+    private BoxCollider swordColl;
+
+    private AttackState attackState;
+    public enum AttackState
+    { 
+        Windup,
+        Impact,
+        Cooldown,
+    };
 
     private void Awake()
     {
         anim = GetComponent<Animator>();
     }
+    private void Start()
+    {
+        if(sword!=null)
+        {
+            swordColl = sword.GetComponent<BoxCollider>();
+            swordColl.enabled = false;
+        }
+    }
+
 
     public void TryToAttack()
     {
@@ -31,8 +51,58 @@ public class MeeleFighter : MonoBehaviour
 
         var animState = anim.GetNextAnimatorStateInfo(1);
 
+        float impactStartTime = 0.3f;
+        float impactEndTime = 0.5f;
+        float normalizedTime = 0f;
+        float elaspedTime = 0f;
+        attackState = AttackState.Windup;
+        while(elaspedTime<animState.length)
+        {
+            elaspedTime += Time.deltaTime;
+            normalizedTime = elaspedTime / animState.length;
+
+            if(attackState==AttackState.Windup)
+            {
+                if(normalizedTime>=impactStartTime)
+                {
+                    attackState = AttackState.Impact;
+                    swordColl.enabled = true;
+                }
+            }
+            else if(attackState==AttackState.Impact)
+            {
+                if(normalizedTime>=impactEndTime)
+                {
+                    attackState = AttackState.Cooldown;
+                    swordColl.enabled = false;
+                }
+            }
+            else if(attackState==AttackState.Cooldown)
+            {
+                //combat
+            }
+            yield return null;
+        }
+        InAction = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("reaction");
+        if (other.tag == "PlayerHitbox" && !InAction)
+        {
+            StartCoroutine(Reaction());
+        }
+    }
+    IEnumerator Reaction()
+    {
+        InAction = true;
+        Debug.Log("dd");
+        anim.CrossFade("Reaction", 0.2f);
+        yield return null;
+
+        var animState = anim.GetNextAnimatorStateInfo(1);
+
         yield return new WaitForSeconds(animState.length);
-        Debug.Log(animState.length);
         InAction = false;
     }
 }
